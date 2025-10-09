50445354ee927cda2eaf950a2906f957d8c88b30
StefanyMikaelle <stefany_mikaelle@hotmail.com>
2025-10-09 05:58:17 -0300

    service

--- src/main/kotlin/com/stefany/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/stefany/mercadolivro/controller/CustomerController.kt
@@ -3,56 +3,45 @@ package com.stefany.mercadolivro.controller
 import com.stefany.mercadolivro.controller.request.PostCustomerRequest
 import com.stefany.mercadolivro.controller.request.PutCustomerRequest
 import com.stefany.mercadolivro.model.CustomerModel
+import com.stefany.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("customer")
-class CustomerController {
+class CustomerController (
 
-    val customers = mutableListOf<CustomerModel>()
+    val customerService: CustomerService
+
+){
+
 
     @GetMapping
     fun getAll(@RequestParam name : String? ) : List<CustomerModel> {
-        name?.let{
-            return customers.filter { it.name.contains(name, true) }
-        }
-        return customers
+       return customerService.getAll(name)
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest)  {
-
-        val id = if(customers.isEmpty()){
-            1
-        } else {
-            customers.last().id.toInt() + 1
-        }.toString()
-
-        customers.add(CustomerModel(id, customer.name, customer.email))
+        return customerService.create(customer)
     }
 
     @GetMapping("/{id}")
     fun getCustomer(@PathVariable id: String): CustomerModel {
-        return customers.filter { it.id == id }.first()
+        return customerService.getCustomer(id)
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun updateCustomer(@PathVariable id: String, @RequestBody customer: PutCustomerRequest) {
-        customers.filter { it.id == id }.first().let{
-            it.name = customer.name
-            it.email = customer.email
-        }
+        return customerService.updateCustomer(id, customer)
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun delete(@PathVariable id: String) {
-        customers.removeIf {
-            it.id == id
-        }
+        return customerService.delete(id)
     }
 
 }
---
+++ src/main/kotlin/com/stefany/mercadolivro/service/CustomerService.kt
@@ -0,0 +1,47 @@
+package com.stefany.mercadolivro.service
+
+import com.stefany.mercadolivro.controller.request.PostCustomerRequest
+import com.stefany.mercadolivro.controller.request.PutCustomerRequest
+import com.stefany.mercadolivro.model.CustomerModel
+import org.springframework.stereotype.Service
+
+@Service
+class CustomerService {
+
+    val customers = mutableListOf<CustomerModel>()
+
+    fun getAll(name : String? ) : List<CustomerModel> {
+        name?.let{
+            return customers.filter { it.name.contains(name, true) }
+        }
+        return customers
+    }
+
+    fun create(customer: PostCustomerRequest)  {
+
+        val id = if(customers.isEmpty()){
+            1
+        } else {
+            customers.last().id.toInt() + 1
+        }.toString()
+
+        customers.add(CustomerModel(id, customer.name, customer.email))
+    }
+
+    fun getCustomer(id: String): CustomerModel {
+        return customers.filter { it.id == id }.first()
+    }
+
+    fun updateCustomer( id: String, customer: PutCustomerRequest) {
+        customers.filter { it.id == id }.first().let{
+            it.name = customer.name
+            it.email = customer.email
+        }
+    }
+
+    fun delete( id: String) {
+        customers.removeIf {
+            it.id == id
+        }
+    }
+}
